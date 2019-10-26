0168638365322bbd0cb884a7a574319fab32fde9
vagrant <vagrant@localhost.localdomain>
2019-10-26 23:35:47 +0900

    ルーティングでexceptの記述をexcept: [:index]に修正した

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   #           PUT    /entries/:id(.:format)            entries#update
   #           DELETE /entries/:id(.:format)            entries#destroy
   resources :blogs do
-    resources :entries, except: {:index}
+    resources :entries, except: [:index]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
